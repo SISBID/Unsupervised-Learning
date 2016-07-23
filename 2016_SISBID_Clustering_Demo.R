6efcc86b42220138af952a6b8a7a067beee5443c
yfliu15 <yfliu@email.unc.edu>
2016-07-23 14:13:25 -0400

    Add files via upload

---
+++ 2016_SISBID_Clustering_Demo.R
@@ -0,0 +1,185 @@
+#############################################################
+#2016 SISBID Module 5 - Unsupervised Learning
+#Genevera I. Allen & Yufeng Liu
+#Clustering Demos for use in lecture
+############################################################
+
+
+
+########################################################
+#Data set 1 - Simulated Data
+#Small simulated data set to demonstrate concepts with k-means clustering
+##########################################################
+
+#simulate data
+n = 300
+mu1 = c(3,3); mu2 = c(7,4); mu3 = c(6,5.5); 
+Sig = matrix(c(1,.5,.5,1),2,2)
+x1 = t(matrix(mu1,2,n/3)) + matrix(rnorm(n),n/3,2)
+xx = matrix(rnorm(n*2/3),n/3,2)
+x2 = t(matrix(mu2,2,n/3)) + xx%*%chol(Sig)
+xx = matrix(rnorm(n*2/3),n/3,2)
+x3 = t(matrix(mu3,2,n/3)) + xx%*%chol(Sig)
+x = rbind(x1,x2,x3)
+Y = c(rep(1,n/3),rep(2,n/3),rep(3,n/3))
+y = factor(Y)
+
+plot(x[,1],x[,2],col=as.numeric(y),pch=16)
+
+
+#try changing k - which clustering looks best?
+k = 3
+km = kmeans(x,centers=k)
+plot(x[,1],x[,2],col=km$cluster,pch=16)
+cens = km$centers
+points(cens[,1],cens[,2],col=1:k,pch=16,cex=3)
+
+
+#code to understand K-means algorithm
+require("animation")
+mv.kmeans = function(x,k,cens=NULL){
+  n = nrow(x)
+  if(is.null(cens)){
+      cens = x[sample(1:n,k),]
+    }
+  plot(x[,1],x[,2],pch=16)
+  points(cens[,1],cens[,2],col=1:k,pch=16,cex=3)
+  thr = 1e-6; ind = 1; iter = 1;
+  while( ind>thr)
+    {
+      oldcen = cens
+      km = kmeans(x,centers=cens,iter.max=1,nstart=1,algorithm="MacQueen")
+      plot(x[,1],x[,2],col=km$cluster,pch=16)
+      points(cens[,1],cens[,2],col=1:k,pch=16,cex=3)
+      cens = km$centers
+      #print(cens)
+      plot(x[,1],x[,2],col=km$cluster,pch=16)
+      points(cens[,1],cens[,2],col=1:k,pch=16,cex=3)
+      ind = sum(diag((oldcen-cens)%*%t(oldcen-cens)))
+      #print(ind)
+    } 
+}
+
+#watch K-means algorithm movie
+#start from random starting points
+saveHTML(mv.kmeans(x,3,cens=NULL),img.name="km1")
+
+
+########################################################
+#Data set 2 - NCI Microarray data
+#Apply K-means to cluster a high-dimensional data set.
+#Apply hierarchical clustering & try out different linkages.
+#Apply biclustering (Cluster heatmap) to visualize data.
+##########################################################
+
+require("ISLR")
+
+ncidat = t(NCI60$data)
+colnames(ncidat) = NCI60$labs
+
+dim(ncidat)
+unique(colnames(ncidat))
+
+############
+#apply K-means
+K = 9
+km = kmeans(t(ncidat),centers=K)
+
+#how do we visualize K-means results?
+
+#PCA - take SVD to get solution
+#center genes, but don't scale
+X = t(scale(t(ncidat),center=TRUE,scale=FALSE))
+sv = svd(t(X));
+U = sv$u
+V = sv$v
+D = sv$d
+Z = t(X)%*%V;
+
+plot(Z[,1],Z[,2],col=km$cluster,type="n")
+text(Z[,1],Z[,2],colnames(ncidat),cex=.75,col=km$cluster)
+cens = km$centers
+points(cens%*%V[,1],cens%*%V[,2],col=1:K,pch=16,cex=3)
+
+#Re-run and see if solution changes
+K = 9
+km = kmeans(t(ncidat),centers=K)
+plot(Z[,1],Z[,2],col=km$cluster,type="n")
+text(Z[,1],Z[,2],colnames(ncidat),cex=.75,col=km$cluster)
+cens = km$centers
+points(cens%*%V[,1],cens%*%V[,2],col=1:K,pch=16,cex=3)
+
+#try different K
+K = 5
+km = kmeans(t(ncidat),centers=K)
+plot(Z[,1],Z[,2],col=km$cluster,type="n")
+text(Z[,1],Z[,2],colnames(ncidat),cex=.75,col=km$cluster)
+cens = km$centers
+points(cens%*%V[,1],cens%*%V[,2],col=1:K,pch=16,cex=3)
+
+
+##################
+#hierarchical clustering
+
+require("ISLR")
+
+ncidat = t(NCI60$data)
+colnames(ncidat) = NCI60$labs
+
+dim(ncidat)
+unique(colnames(ncidat))
+
+#complete linakge - Euclidean distance
+cols = as.numeric(as.factor(colnames(ncidat)))
+Dmat = dist(t(ncidat))
+com.hclust = hclust(Dmat,method="complete")
+plot(com.hclust,cex=.7,main="Complete Linkage")
+
+#single linakge
+dev.new()
+sing.hclust = hclust(Dmat,method="single")
+plot(sing.hclust,cex=.7,main="Single Linkage")
+
+#average linakge
+dev.new()
+ave.hclust = hclust(Dmat,method="average")
+plot(ave.hclust,cex=.7,main="Average Linkage")
+
+#Ward's linakge
+dev.new()
+ward.hclust = hclust(Dmat,method="ward.D")
+plot(ward.hclust,cex=.7,main="Ward's Linkage")
+
+#complete linkage with different distances
+dev.new()
+Dmat = dist(t(ncidat),method="manhattan") #L1 distance
+com.hclust = hclust(Dmat,method="complete")
+plot(com.hclust,cex=.7,main="Complete Linkage - L1 Dist")
+
+
+##########
+#Biclustering - Cluster Heatmap 
+
+require("ISLR")
+ncidat = t(NCI60$data)
+colnames(ncidat) = NCI60$labs
+
+#filter genes using PCA
+X = t(scale(t(ncidat),center=TRUE,scale=FALSE))
+sv = svd(t(X));
+V = sv$v
+
+#PC loadings - visualize data by limiting to top genes in magnitude in the PC loadings 
+aa = grep("grey",colors())
+bb = grep("green",colors())
+cc = grep("red",colors())
+gcol2 = colors()[c(aa[1:30],bb[1:20],rep(cc,2))]
+
+j = 2
+ord = order(abs(V[,j]),decreasing=TRUE)
+x = as.matrix(X[ord[1:250],])
+
+#cluster heatmap - uses Ward's linkage (complete is default)
+heatmap(x,col=gcol2,hclustfun=function(x)hclust(x,method="ward.D"))
+
+######################################################
---
+++ 2016_SISBID_UnsupLearning_Lab.R
@@ -0,0 +1,266 @@
+#############################################################
+#2016 SISBID Module 5 - Unsupervised Learning
+#Genevera I. Allen & Yufeng Liu
+#Unsupervised Lab
+############################################################
+
+
+####################
+#Data Description
+#############
+#gdat - Gene Expression Data - n = 445 patients x p = 353 genes
+#Only 353 genes with somatic mutations from COSMIC are retained
+#Data is Level III TCGA BRCA RNA-Sequencing gene expression data that have already been pre-processed according to the following steps:
+#1) reads normalized by RPKM
+#2) corrected for overdispersion by a log-transformation (1 + data)
+#Short gene name labels are given as the column names
+###############
+#cdat - Clinical Data - n = 445 patients x q = 6 clinical features
+#1) Subtype - denotes 5 PAM50 subtypes including Basal-like, Luminal A, Luminal B, HER2-enriched, and Normal-like
+#2) ER-Status - estrogen-receptor status
+#3) PR-Status - progesterone-receptor status
+#4) HER2-Status - human epidermal growth factor receptor 2 status
+#5) Node - number of lymph nodes involved
+#6) Metastasis - indicator for whether the cancer has metastasized
+###############
+
+
+############
+#Problem 1 - Dimension reduction 
+############
+#Problem 1a - Apply PCA, NMF, ICA and MDS to this dataset. Compare and contrast the results using these methods.  
+
+#Problem 1b - Relate the dimension reduction results with the clinical data. Is any clinical information reflected in the lower dimensional spaces?
+
+#Problem 1c - Overall, which dimension reduction method do you recommend for this data set and why?
+
+############
+#Problem 2 - Clustering 
+############
+#Problem 2a - Apply various clustering algorithms such as K-means (explore different K), hierarchical clustering (explore different linkages), NMF, and biclustering. Compare the clustering results using these methods.
+
+#Problem 2b - Relate the clustering results with the clinical data. Can the clustering algorithm recover some of the clinical information such as cancer subtypes?
+
+#Problem 2c - Overall, which clustering method(s) do you recommend for this data set and why?
+
+############
+#Problem 3 - Multiple comparisons 
+############
+#Problem 3a - Identify important genes to differetiate ER postive and negative, PR postive and negative, HER2 postive and negative, metastasis status.
+
+#Problem 3b - Try different procedures to adjust for multiple comparisons.
+
+#Problem 3c - Examine the lists of genes identified using different methods for each clinical response.  Which method is best?  Why?
+
+############
+#Problem 4 - Graphical models
+############
+
+# Use graphical models to explore interactions among genes.  Are any of the well-connected genes related to patterns previously identified? 
+
+
+##################
+#Problem 5 - Visulaization
+#################
+
+#Visualize this data using multiple approaches.
+
+#Prepare the "best" visual summary of this data.
+
+
+################
+#Problem 6 - Exploratory Data Analysis Summary
+##################
+
+#What is the most interesting finding?
+
+#Is this finding consistent and stable?
+
+#Prepare a visual summary that best illustrates this interesting finding.
+
+
+###############################################################
+###############################################################
+#R scripts to help out with the BRCA case study Lab
+#Don't peek at this if you want to practice coding on your own!!
+##################################################################
+
+#load data
+load("UnsupL_SISBID_2016.RData")
+
+#explore data
+dim(gdat)
+dim(cdat)
+
+table(cdat$Subtype)
+table(cdat$ER)
+table(cdat$PR)
+table(cdat$HER2)
+table(cdat$Node)
+table(cdat$Metastasis)
+table(cdat$ER,cdat$PR)
+
+hist(c(gdat),breaks=100)
+
+################
+#visualize data
+
+#cluster heatmap - biclustering
+aa = grep("grey",colors())
+bb = grep("green",colors())
+cc = grep("red",colors())
+gcol2 = colors()[c(aa[1:2],bb[1:25],cc[1:50])]
+
+# without scaling
+heatmap(gdat,col=gcol2,hclustfun=function(x)hclust(x,method="ward.D"))
+# with scaling
+heatmap(scale(gdat),col=gcol2,hclustfun=function(x)hclust(x,method="ward.D"))
+
+Cols=function(vec){cols=rainbow(length(unique(vec)))
+                   return(cols[as.numeric(as.factor(vec))])}
+
+heatmap(scale(gdat),col=gcol2,hclustfun=function(x)hclust(x,method="ward.D"),labRow=cdat$Subtype,RowSideColors=Cols(cdat$Subtype))
+
+
+####################
+#pattern recognition
+#dimension reduction
+
+Cols=function(vec){cols=rainbow(length(unique(vec)))
+                   return(cols[as.numeric(as.factor(vec))])}
+
+
+#PCA
+sv = svd(gdat)
+V = sv$v
+Z = gdat%*%V
+
+K = 3
+pclabs = c("PC1","PC2","PC3","PC4")
+par(mfrow=c(1,K))
+for(i in 1:K){
+  j = i+1
+  plot(Z[,i],Z[,j],pch=16,xlab=pclabs[i],ylab=pclabs[j],col=Cols(cdat$Subtype))
+}
+legend(-30,-20,pch=16,col=rainbow(5),levels(cdat$Subtype))
+
+#MDS 
+dev.off()
+Dmat = dist(gdat)
+mdsres = cmdscale(Dmat,k=2)
+plot(mdsres[,1],mdsres[,2],pch=16,col=Cols(cdat$Subtype))
+legend(-20,30,pch=16,col=rainbow(5),levels(cdat$Subtype))
+
+
+#NMF
+require("NMF")
+
+K = 4
+nmffit = nmf(gdat,rank=K) 
+W = basis(nmffit)
+H = coef(nmffit)
+
+kk = 3
+pclabs = c("NMF1","NMF2","NMF3","NMF4")
+par(mfrow=c(1,kk))
+for(i in 1:kk){
+  j = i+1
+  plot(W[,i],W[,j],pch=16,xlab=pclabs[i],ylab=pclabs[j],col=Cols(cdat$Subtype))
+}
+legend(0,65,pch=16,col=rainbow(5),levels(cdat$Subtype))
+
+par(mfrow=c(1,2))
+basismap(nmffit,annRow=cdat$Subtype,scale="col",legend=FALSE)
+coefmap(nmffit,annCol=colnames(gdat),scale="col",legend=FALSE)
+
+
+#ICA
+require("fastICA")
+
+K = 4
+icafit = fastICA(gdat,n.comp=K)
+
+kk = 3
+pclabs = c("ICA1","ICA2","ICA3","ICA4")
+par(mfrow=c(1,kk))
+for(i in 1:kk){
+  j = i+1
+  plot(icafit$A[i,],icafit$A[j,],pch=16,xlab=pclabs[i],ylab=pclabs[j],col=Cols(cdat$Subtype))
+}
+legend(-4,-2,pch=16,col=rainbow(5),levels(cdat$Subtype))
+
+#######################
+#clustering
+
+#K-means
+K = 5
+km = kmeans(gdat,centers=K)
+table(km$cluster,cdat$Subtype)
+
+
+#hierarchical
+#which linakges is the best?
+#which distance metric is the best?
+
+Dmat = dist(gdat)
+com.hc = hclust(Dmat,method="ward.D")
+
+dev.off()
+plot(com.hc,labels=cdat$Subtype,cex=.5)
+
+res.com = cutree(com.hc,5)
+table(res.com,cdat$Subtype)
+
+
+#################
+#genes significantly associated with ER or PR Status, etc
+
+x = gdat[cdat$ER=="Positive" | cdat$ER=="Negative",]
+y.er = cdat$ER[cdat$ER=="Positive" | cdat$ER=="Negative"]
+y.label<-rep(1, length(y.er))
+y.label[y.er == "Positive"]=2
+
+
+ps <- NULL
+for(i in 1:ncol(gdat)) ps <- c(ps,
+ t.test(x[y.label==1,i],x[y.label==2,i])$p.value)
+fdrs.bh <- p.adjust(ps, method="BH")
+
+cat("Number of Tests significant with alpha=0.1 using Bonferroni correction:",
+sum(ps<0.1/length(y.label)), fill=TRUE)
+
+cat("Number of Tests with FDR below 0.1:",
+sum(fdrs.bh<0.1), fill=TRUE)
+
+plot(sort(ps,decreasing=FALSE),ylab="P-Values")
+#BH procedure
+abline(a=0, b=0.1/length(y.label),col="red")
+#Bonferroni
+abline(a=0.1/length(y.label), b=0,col="blue")
+
+
+#SAM
+library(samr)
+
+# samfit x: p by n
+samfit<-SAM(t(x),y.label,resp.type="Two class unpaired", fdr.output=0.1)    
+
+# examine significant gene list 
+ 
+print(samfit)
+ # plot results
+plot(samfit)      
+
+
+#######################
+#graphical models - how are genes related?
+
+#this takes a bit of time
+require("igraph")
+require("XMRF")
+
+lam = lambdaMax(gdat)*sqrt(log(ncol(gdat))/nrow(gdat))*0.01
+net = XMRF(t(gdat),method="LPGM",lambda.path=lam,N=1,th=.0025)
+plot(net)
+
+
